2b6c6088ed8f95b5edc001b496725fc53a16137d
PowerMX <powermx@gmail.com>
2023-02-24 16:19:24 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# telegramgpt
-Telegram
+# ChatGPT_TelegramBot
+ Example Bot [TanyaAkuDong](https://t.me/TanyaAkuDong_bot)   
+ ChatGPT Telegram Chat Bot with Nodejs   
+ Rename file .env.example to .env and insert your bot **TELEGRAM_TOKEN** and **OPENAI_TOKEN**
+
+# download or clone this repository and install bot
+```
+git clone https://github.com/msyahrul801/ChatGPT_TelegramBot.git
+```
+```
+npm install
+```
+# run bot
+```
+npm start
+```
+
+# using Railway Free Hosting
+1. Register Here [Railway](https://railway.app/) 
+2. Clone this repository to your github
+3. Create a new project from your github and set your variable **TELEGRAM_TOKEN** and **OPENAI_TOKEN**
+4. done
---
+++ index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.OPENAI_TOKEN,
+}));
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1];
+    bot.sendMessage(chatId, resp);
+});
+
+bot.on("message", async (msg) => {
+    let chatId = msg.chat.id;
+    let message = msg.text;
+    if(message == '/start'){
+        return bot.sendMessage(chatId, "Bienvenido y siéntete libre de preguntarme cualquier cosa.\n\nDudas o comentarios: @vpnmx");
+    }
+    let messageBot = await bot.sendMessage(chatId, "Buscando ...");
+    try{
+        let response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: message,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        await bot.deleteMessage(chatId,messageBot.message_id);
+        await bot.sendMessage(chatId, response.data.choices[0].text);
+    }catch(err){
+        await bot.deleteMessage(chatId,messageBot.message_id);
+        await bot.sendMessage(chatId, "Error"); 
+    }
+});
